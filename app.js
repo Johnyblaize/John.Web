e85a17a6a342cf4fc635200dad1174e628c9d107
John <johnumoru405@gmail.com>
2025-08-18 17:08:48 +0100

    john first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let name = document.querySelector('hi.name');
+let courseTitle = document.getElementById("course-title");
+
+const toggleBtn = document.getElementById("menu-bar");
+
+const navLinks = document.getElementById
